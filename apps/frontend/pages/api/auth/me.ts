cfed43937282d769e1d070abe0cea1c6eb9fed9d
Ove Andersen <ove.andersen@outlook.com>
2022-12-14 11:01:49 +0100

    Removed peer dependency

--- apps/frontend/pages/api/auth/me.ts
+++ apps/frontend/pages/api/auth/me.ts
@@ -1,3 +1,4 @@
 import { Auth0ProfileHandler, ProfileHandler } from "lib";
+import type {} from "@auth0/nextjs-auth0"; // To make TS happy: https://github.com/microsoft/TypeScript/issues/47663#issuecomment-1270716220
 
 export default Auth0ProfileHandler(ProfileHandler).handleProfile;
